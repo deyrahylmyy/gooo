748f0135be6eb3a4dc628ddc38206f704b67ebcd
Petani Kode <contoh@petanikode.com>
2023-05-21 13:16:59 +0700

    materi add

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -15,12 +15,7 @@ const Hero = () => {
             <HeroText>
             - Dinamis dan penuh solusi, kami adalah perusahaan media teknologi yang berusaha untuk yang terbaik dan ingin memahami apa yang diinginkan klien. Kami menawarkan solusi TI untuk membantu Anda menghadapi masa depan dan bersiap menghadapi hal yang tidak terduga.
             </HeroText>
-            <ButtonWrapper>
-                <Link to='signup'>
-                    <Button>Get Started</Button>
-                </Link>
-                <HeroButton>Find More</HeroButton>
-            </ButtonWrapper>
+
         </Container>
     </HeroSection>
   )
